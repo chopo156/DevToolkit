fdda35eee6d5bf5586fd6f22a50d031ef9f9ba85
Hannele Ruiz <justlemoncl@gmail.com>
2020-05-24 15:50:09 -0400

    Added basic script

---
+++ DevToolkit/Script.cs
@@ -0,0 +1,23 @@
+#if SINGLEPLAYER
+using GTA;
+#elif FIVEM
+using Script = CitizenFX.Core.BaseScript;
+#endif
+
+namespace DevToolkit
+{
+    /// <summary>
+    /// Script used by DevToolkit to provide tools.
+    /// </summary>
+    public class DevToolkit : Script
+    {
+        #region Constructor
+
+        public DevToolkit()
+        {
+
+        }
+
+        #endregion
+    }
+}
