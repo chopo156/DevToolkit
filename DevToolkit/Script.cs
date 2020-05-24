ffb543701cbc76bd2083e32f0b033ebd4862ea3c
Hannele Ruiz <justlemoncl@gmail.com>
2020-05-24 18:28:53 -0400

    Added basic command system for SP

--- DevToolkit/Script.cs
+++ DevToolkit/Script.cs
@@ -1,6 +1,9 @@
 #if SINGLEPLAYER
 using GTA;
+using System;
+using System.Reflection;
 #elif FIVEM
+using CitizenFX.Core;
 using Script = CitizenFX.Core.BaseScript;
 #endif
 
@@ -11,13 +14,34 @@ namespace DevToolkit
     /// </summary>
     public class DevToolkit : Script
     {
-        #region Constructor
-
         public DevToolkit()
         {
-
+#if SINGLEPLAYER
+            // For Singleplayer, we need to add the commands and ticks manually
+            Tick += DevToolkit_Tick;
+#endif
         }
 
-        #endregion
+#if SINGLEPLAYER
+        private void DevToolkit_Tick(object sender, EventArgs e)
+        {
+            // Get all of the methods
+            MethodInfo[] methods = GetType().GetMethods();
+
+            // And iterate over them
+            foreach (MethodInfo method in methods)
+            {
+                // If there is a command attribute
+                if (method.GetCustomAttribute(typeof(CommandAttribute)) is CommandAttribute command)
+                {
+                    // If the command was entered as a cheat, execute it
+                    if (Game.WasCheatStringJustEntered(command.Command))
+                    {
+                        method.Invoke(this, new object[0]);
+                    }
+                }
+            }
+        }
+#endif
     }
 }
